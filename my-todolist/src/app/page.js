367fc2e6cc9a721b880246b2edee6f5cd7e2c4dc
rnZolo <rjsagge@gmail.com>
2023-05-26 21:33:06 +0800

    few changes on boiler plate

--- my-todolist/src/app/page.js
+++ my-todolist/src/app/page.js
@@ -1,17 +1,12 @@
 import { React } from 'react';
-
-function appUI() {
-  return (
-    <div className='w-4/5 h-[100px] p-2'>
-
-    </div>
-  )
-}
+import './globals.css';
 
 export default function Home() {
   return (
-    <main className="grid min-h-screen place-item-center p-24">
-      <appUI />
+    <main className="grid min-h-full w-full place-content-center p-2">
+      <div className=' main-interface w-full h-[100px] p-2 bg-vio-gray-600'>
+        <h1 className='text-xl text-white'> This would be a header</h1>
+      </div>
     </main>
   )
 }
--- my-todolist/tailwind.config.js
+++ my-todolist/tailwind.config.js
@@ -6,12 +6,47 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'vio-gray': '#6374ae',
+      'pastel-green': '#69d24b',
+    },
+    screens: {
+      '2xdextop': { 'max': '1600px' },
+      'xdesktop': { 'max': '1279px' },
+      'desktop': { 'max': '1023px' },
+      'tablet': { 'max': '767px' },
+      'xmob': { 'max': '639px' },
+      'mob': { 'max': '459px' },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'vio-gray': {
+          '50': '#f5f6fa',
+          '100': '#eaebf4',
+          '200': '#d0d4e7',
+          '300': '#a6b0d3',
+          '400': '#7686ba',
+          '500': '#6374ae',
+          '600': '#414f88',
+          '700': '#36406e',
+          '800': '#30395c',
+          '900': '#2c324e',
+          '950': '#1d2034',
+        },
+        'pastel-green': {
+          '50': '#effbea',
+          '100': '#dbf6d1',
+          '200': '#baeea8',
+          '300': '#8ee274',
+          '400': '#69d24b',
+          '500': '#48b72b',
+          '600': '#34921e',
+          '700': '#2a6f1c',
+          '800': '#25591b',
+          '900': '#214c1b',
+          '950': '#0e290a',
+        },
+      }
     },
   },
   plugins: [],
