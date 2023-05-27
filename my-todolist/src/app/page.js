45467ce0b31f6697351ac4b247a8fdb55a8871d9
rnZolo <rjsagge@gmail.com>
2023-05-27 21:49:31 +0800

    nothing

--- my-todolist/src/app/page.js
+++ my-todolist/src/app/page.js
@@ -3,10 +3,16 @@ import './globals.css';
 
 export default function Home() {
   return (
-    <main className="grid min-h-full w-full place-content-center p-2">
-      <div className=' main-interface w-full h-[100px] p-2 bg-vio-gray-600'>
-        <h1 className='text-xl text-white'> This would be a header</h1>
+    <main className="flex flex-col justify-center items-center h-full w-full  p-2">
+      <div className=' main-interface relative overflow-hidden w-[80%] h-[100px] rounded-md p-2 bg-vio-gray-600'>
+        <div className='oval-bg w-full h-full z-0 -rotate-12 scale-125 rounded-t-[50%] translate-x-16 bg-vio-gray-500 absolute right-0 bottom-[-80px]'></div>
+        <div className='wrapper h-full w-full'>
+          <h1 className='uppercase text-xl text-white t indent-5'>Add To do :</h1>
+          <div className='user-action w-full h-full flex z-100'>
+            <input type='text' className='z-100'></input>
+          </div>
+        </div>
       </div>
-    </main>
+    </main >
   )
 }
