ae4368a2771f7669381a292315f8143cf630fd24
rnZolo <rjsagge@gmail.com>
2023-05-29 21:23:42 +0800

    change to static shop website

---
+++ my-todolist/src/app/components/Hero.js
@@ -0,0 +1,29 @@
+import { React } from "react";
+
+function Hero() {
+
+    return (
+        <section className="banner-section w-[80%] h-fit flex flex-col gap-[20px] max-w-[1200px]">
+            <div className="banner-h-products upper  h-[400px] flex justify-center items-center">
+                this will contain a horizontal slider of milktea banner
+            </div>
+            <div className="lower flex gap-4">
+                <nav className="banner-aside-nav w-[150px]">
+                    banner as nav that will change the h-banner and the content
+                </nav>
+                <div className="banner-content w-[500px]">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum.
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export { Hero };
--- my-todolist/src/app/page.js
+++ my-todolist/src/app/page.js
@@ -1,18 +1,11 @@
 import { React } from 'react';
+import { Hero } from './/components/Hero';
 import './globals.css';
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center h-full w-full  p-2">
-      <div className=' main-interface relative overflow-hidden w-[80%] h-[100px] rounded-md p-2 bg-vio-gray-600'>
-        <div className='oval-bg w-full h-full z-0 -rotate-12 scale-125 rounded-t-[50%] translate-x-16 bg-vio-gray-500 absolute right-0 bottom-[-80px]'></div>
-        <div className='wrapper h-full w-full'>
-          <h1 className='uppercase text-xl text-white t indent-5'>Add To do :</h1>
-          <div className='user-action w-full h-full flex z-100'>
-            <input type='text' className='z-100'></input>
-          </div>
-        </div>
-      </div>
-    </main >
+    <div className='w-full h-screen flex justify-center'>
+      <Hero />
+    </div>
   )
 }
